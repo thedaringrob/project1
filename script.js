b6f3aeb55a8c67bf4aa8c62417da84e048d3e561
Robert Crozier <crozierr2@gmail.com>
2016-07-21 16:12:39 -0400

    board and css, need jscript

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+(function(){
+  console.log('it works')
+
+//enemy selector here
+    //need iterator to select randomly
+
+
+//button manipulators
+  //random percent heal
+  //random percent damage
+  //random percent heavy damage
+
+  //platinum edition--make button appear if
+  //2 consecutive critical hits--insta kill!
+
+  //loop through is it a win?
+
+
+
+//win counter--is win count 3? y? you win!
+//not 3? keep playing
+
+
+//event listeners tied to actions here
+
+})();
